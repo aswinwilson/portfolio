2a08c27acfdcac46074fe84ed59aaf9ff0aa04e1
Aswin Antony Wilson <39756779+aswinwilson@users.noreply.github.com>
2021-05-09 22:06:09 -0400

    Check fix

---
+++ src/components/AboutMe.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-4">
+          <div className="aboutme-img-wrapper">
+            <img src="" alt="Aswin Antony Wilson"></img>
+          </div>
+        </div>
+        <div className="col-8">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AboutMe
