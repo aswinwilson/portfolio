481c6f048e6b532b5e7e5bc010bfe8ecdf89dcb1
Aswin Antony Wilson <39756779+aswinwilson@users.noreply.github.com>
2021-05-10 10:58:33 -0400

    cname fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className="container">
-                <div>"I can do everything through the lord who strengthens me." - Philippians 4:13</div>
+                <div className="text-center">"I can do everything through the lord who strengthens me." - Philippians 4:13</div>
             </div>
         </div>
     )
