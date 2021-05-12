19a4f464dfb3530a0334fe9971e58248aa79c195
Aswin Antony Wilson <39756779+aswinwilson@users.noreply.github.com>
2021-05-12 16:18:58 -0400

    About me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <>
-      <div className="row">
-        <div className="col-4">
-          <div className="aboutme-img-wrapper">
-            <img src="" alt="Aswin Antony Wilson"></img>
+    <div className="aboutme-wrapper">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-4">
+            <div className="aboutme-img-wrapper">
+              <h3>Aswin Wilson</h3>
+              <h4>Software Developer</h4>
+            </div>
+          </div>
+          <div className="col-8">
+            <div>
+              I'm a Software Developer with almost 2 years of professional experience in Front End Development.
+              I always try to learn new technologies to enhance my skills and develop my knowledge in the domain.
+          </div>
           </div>
         </div>
-        <div className="col-8">
-
-        </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import ShareButtons from "./ShareButtons";
+import AboutMe from "./AboutMe";
 
 function PageContent() {
   return (
     <div className="page-content">
       <Header />
       <ShareButtons/>
+      <AboutMe/>
     </div>
   );
 }
